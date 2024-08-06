32416422d1e8227c6365002c25b35582e6c8b6bc
Muehehe1 <darkrazo400@gmail.com>
2024-08-06 21:23:18 +0800

    Added ProductPage. Renamed and deleted other assets

--- src/app/shop/page.jsx
+++ src/app/shop/page.jsx
@@ -11,49 +11,114 @@ import {
 } from "@/components/ui/card"
 import { Star, ShoppingCart } from 'lucide-react';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Image from 'next/image'
 
 export const ProductList = [
     {
         id: 0,
         name: 'Blade of Olympus - God of War',
         price:'₱5000',
-        imageSrc:'./assets/ProductList/item1.png',
+        imageSrc:'/assets/ProductList/item1.png',
         href: 'Blade of Olympus God of War'
     },
     {
         id: 1,
         name: 'Iron Man Suit Full Body Armor - Iron Man',
         price:'₱50000',
-        imageSrc:'./assets/ProductList/item2.png',
+        imageSrc:'/assets/ProductList/item2.png',
         href: 'Iron Man Suit Full Body Armor Iron Man'
     },
     {
         id: 2,
         name: 'The Dominator - Psycho-Pass',
         price:'₱9000',
-        imageSrc:'./assets/ProductList/item3.png',
+        imageSrc:'/assets/ProductList/item3.png',
         href: 'The Dominator Psycho-Pass'
     },
     {
         id: 3,
         name: 'Chainsaw - Chainsaw Man',
         price:'₱5000',
-        imageSrc:'./assets/ProductList/item4.png',
+        imageSrc:'/assets/ProductList/item4.png',
         href: 'Chainsaw Chainsaw Man'
     },
     {
         id: 4,
         name: 'ODM Gear - Attack on Titan',
         price:'₱8000',
-        imageSrc:'./assets/ProductList/item5.png',
+        imageSrc:'/assets/ProductList/item5.png',
         href: 'ODM Gear Attack on Titan'
     },
     {
         id: 5,
         name: 'Hylian Shield - Legend of Zelda',
         price:'₱2000',
-        imageSrc:'./assets/ProductList/item6.png',
+        imageSrc:'/assets/ProductList/item6.png',
         href: 'Hylian Shield Legend of Zelda'
+    },
+    {
+        id: 6,
+        name: 'Kaigaku Sword - Demon Slayer',
+        price: '₱1000',
+        imageSrc: '/assets/ProductList/item7.png',
+        href: 'Kaigaku Sword Demon Slayer'
+    },
+    {
+        id: 7,
+        name: 'Ebony and Ivory - Devil May Cry',
+        price: '₱2000',
+        imageSrc: '/assets/ProductList/item8.png',
+        href: 'Ebony and Ivory Devil May Cry'
+    },
+    {
+        id: 8,
+        name: 'Cannon Mina Ashiro - Kaiju No.8',
+        price: '₱2000',
+        imageSrc: '/assets/ProductList/item9.png',
+        href: 'Cannon Mina Ashiro Kaiju No.8'
+    },
+    {
+        id: 9,
+        name: 'Staff of Homa - Genshin Impact',
+        price: '₱2000',
+        imageSrc: '/assets/ProductList/item10.png',
+        href: 'Staff of Homa Genshin Impact'
+    },
+    {
+        id: 10,
+        name: 'Divine Axe Rhitta - Seven Deadly Sins',
+        price: '₱2000',
+        imageSrc: '/assets/ProductList/item11.png',
+        href: 'Divine Axe Rhitta Seven Deadly Sins'
+    },
+    {
+        id: 11,
+        name: 'VCT LOCK//IN Knife - Valorant',
+        price: '₱2000',
+        imageSrc: '/assets/ProductList/item12.png',
+        href: 'VCT LOCKIN Knife Valorant'
+    },
+    {
+        id: 12,
+        name: 'Sentinel of Light Vandal - Valorant',
+        price: '₱5000',
+        imageSrc: '/assets/ProductList/item13.png',
+        href: 'Sentinel of Light Vandal Valorant'
+    },
+    {
+        id: 13,
+        name: 'Saw Cleaver - Bloodborne',
+        price: '₱7000',
+        imageSrc: '/assets/ProductList/item14.png',
+        href: 'Saw Cleaver Bloodborne'
+    },
+    {
+        id: 14,
+        name: 'Moon Stick Wand - Sailor Moon',
+        price: '₱2000',
+        imageSrc: '/assets/ProductList/item15.png',
+        href: 'Moon Stick Wand Sailor Moon'
     }
 ];
 
@@ -89,29 +154,36 @@ const ProductCards = () => {
                                 >
                                 <Card key={item.id} className='w-full h-[400px] bg-[#F9FAFC] card-no-radius card-no-border'>
                                     <CardHeader className='p-0'>
-                                        <img src={item.imageSrc} alt={item.name} className='w-full h-[289px] object-cover m-0'/>
-                                            <CardTitle className='font-light text-[18px] truncate ml-6'>
-                                                {item.name}
-                                            </CardTitle>
-                                            <CardDescription className='text-custom-purple ml-6'>
-                                                {item.price}
-                                            </CardDescription>
-                                        </CardHeader>
+                                        <Image
+                                            src={item.imageSrc}
+                                            width={500}
+                                            height={500}
+                                            alt={item.name}
+                                            priority={true}
+                                            className="w-full h-[289px] object-cover m-0"                                 
+                                        />
+                                        <CardTitle className='font-light text-[18px] truncate mx-6'>
+                                            {item.name}
+                                        </CardTitle>
+                                        <CardDescription className='text-custom-purple ml-6'>
+                                            {item.price}
+                                        </CardDescription>
+                                    </CardHeader>
 
 
-                                        <CardFooter className='flex items-center justify-between pt-4'>
-                                            <div className='flex items-center gap-1 group'>
-                                                <Star className='text-yellow-500 fill-yellow-500' />
-                                                <span className='font-light'>4.8</span>
-                                            </div>
+                                    <CardFooter className='flex items-center justify-between pt-4'>
+                                        <div className='flex items-center gap-1 group'>
+                                            <Star className='text-yellow-500 fill-yellow-500' />
+                                            <span className='font-light'>4.8</span>
+                                        </div>
 
-                                            <div className='flex items-center gap-1 group'>
-                                                <ShoppingCart className='transition-colors duration-300 group-hover:text-custom-purple cursor-pointer' />
-                                                <span className='font-light transition-colors duration-300 group-hover:text-custom-purple cursor-pointer'>
-                                                Add to Cart
-                                                </span>
-                                            </div>
-                                        </CardFooter>
+                                        <div className='flex items-center gap-1 group'>
+                                            <ShoppingCart className='transition-colors duration-300 group-hover:text-custom-purple cursor-pointer' />
+                                            <span className='font-light transition-colors duration-300 group-hover:text-custom-purple cursor-pointer'>
+                                            Add to Cart
+                                            </span>
+                                        </div>
+                                    </CardFooter>
 
                                 </Card>
                                 </Link>
@@ -120,8 +192,7 @@ const ProductCards = () => {
                         </div>                
                     </div>
                 </div>
-                
-                
+            <Footer/>      
         </div>
     )
 }
