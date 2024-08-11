509c6b3b84f70a7bccd81f8289374137d806f722
Muehehe1 <darkrazo400@gmail.com>
2024-08-11 22:24:26 +0800

    Added MyPurchasePage & Checkout Data Adjustment

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,9 @@
 // app/user-profile/page.jsx
 import React from 'react';
 import { auth, currentUser } from "@clerk/nextjs/server";
+import MyPurchasePage from './MyPurchasePage.jsx';
+import { ShoppingCart, Bell, Tag } from 'lucide-react'; // Import Lucide icons
+import Link from 'next/link.js';
 
 export default async function UserProfile() {
   const { userId } = auth();
@@ -30,10 +33,44 @@ export default async function UserProfile() {
   };
 
   return (
-    <div>
-        <h1>{userData.firstName}</h1>
-        <h1>{userData.lastName}</h1>
-        <h1>{userData.email}</h1>
+    <div className="flex h-screen">
+      {/* User Info Section */}
+      <div className="flex-shrink-0 w-1/4 p-6 border-r border-gray-300">
+        <div className="flex items-center mb-6">
+          <img src={userData.image} alt="User Image" className="w-16 h-16 rounded-full mr-4" />
+          <div>
+            <h1 className="text-md font-bold">{userData.firstName} {userData.lastName}</h1>
+            <p className="text-sm text-gray-600">{userData.email}</p>
+          </div>
+        </div>
+
+        {/* Additional Links */}
+        <div className="space-y-4">
+          <div className="flex items-center">
+            <ShoppingCart className="w-5 h-5 mr-2 text-gray-600" />
+            <span className="category-underline cursor-pointer hover:text-custom-purple">
+              My Purchases
+            </span>
+          </div>
+          <div className="flex items-center">
+            <Bell className="w-5 h-5 mr-2 text-gray-600" />
+            <span className="category-underline cursor-pointer hover:text-custom-purple">
+              Notifications
+            </span>
+          </div>
+          <div className="flex items-center">
+            <Tag className="w-5 h-5 mr-2 text-gray-600" />
+            <span className="category-underline cursor-pointer hover:text-custom-purple">
+              Vouchers
+            </span>
+          </div>
+        </div>
+      </div>
+
+      {/* Purchase Page Section */}
+      <div className="flex-grow p-6">
+        <MyPurchasePage userData={userData} />
+      </div>
     </div>
   );
 }
